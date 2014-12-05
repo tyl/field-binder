9411655e90ea0924860cd4d1d8aba74a68aefa69
Edoardo Vacchi <uncommonnonsense@gmail.com>
2014-12-05 13:09:32 +0100

    Download and Install Script

--- download-and-install.sh
+++ download-and-install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+echo Cloning and Installing Maddon Snapshot...
 git clone https://github.com/mstahv/maddon
+cd maddon 
 mvn install -DskipTests
-echo You can now start the demo
+
+echo Cloning and Installing MongoDB Container Snapshot...
+git clone https://github.com/tyl/mongodbcontainer-addon
+
+cd mongodbcontainer-addon
+mvn install -DskipTests
+
+echo
+echo
+echo "You can now start the demo (remember to start mongod)"
