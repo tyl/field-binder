084c419851fb9b7b992a1d2d0efa10744f053361
Edoardo Vacchi <uncommonnonsense@gmail.com>
2014-12-22 12:05:23 +0100

    Updated with Copyright Notice, license

--- field-binder/src/main/java/org/tylproject/vaadin/addon/fieldbinder/ListTable.java
+++ field-binder/src/main/java/org/tylproject/vaadin/addon/fieldbinder/ListTable.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2014 - Tyl Consulting s.a.s.
+ *
+ *   Authors: Edoardo Vacchi
+ *   Contributors: Marco Pancotti, Daniele Zonca
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.tylproject.vaadin.addon.fieldbinder;
 
 import com.vaadin.data.Property;
@@ -168,7 +187,7 @@ public class ListTable<T> extends CustomField<List<T>> {
      * @return adds a default button bar to the bottom right of this component
      */
     public ListTable<T> withDefaultEditorBar() {
-        CrudButtonBar buttonBar = buildDefaultCrudBar();
+        CrudButtonBar buttonBar = buildDefaultEditorBar();
         compositionRoot.setSizeFull();
 
         HorizontalLayout inner = new HorizontalLayout(buttonBar);
@@ -184,7 +203,7 @@ public class ListTable<T> extends CustomField<List<T>> {
      * build and returns a default button bar for this component
      * @return
      */
-    public CrudButtonBar buildDefaultCrudBar() {
+    public CrudButtonBar buildDefaultEditorBar() {
         return new CrudButtonBar(getNavigation().withDefaultBehavior());
     }
 
