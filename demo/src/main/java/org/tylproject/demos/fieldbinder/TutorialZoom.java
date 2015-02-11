35aad1c1eba7841f6f6362d6d4d309bbbee5903e
Edoardo Vacchi <uncommonnonsense@gmail.com>
2015-02-11 09:44:02 +0100

    Update Zoom Tutorial

--- demo/src/main/java/org/tylproject/demos/fieldbinder/TutorialZoom.java
+++ demo/src/main/java/org/tylproject/demos/fieldbinder/TutorialZoom.java
@@ -1,11 +1,14 @@
 package org.tylproject.demos.fieldbinder;
 
+import com.vaadin.data.Property;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Label;
 import org.tylproject.demos.fieldbinder.model.Person;
 import org.tylproject.vaadin.addon.fields.zoom.GridZoomDialog;
 import org.tylproject.vaadin.addon.fields.zoom.TableZoomDialog;
 import org.tylproject.vaadin.addon.fields.zoom.TextZoomField;
+import org.tylproject.vaadin.addon.fields.zoom.ZoomField;
 import org.vaadin.spring.UIScope;
 import org.vaadin.spring.navigator.VaadinView;
 import org.vaadin.viritin.FilterableListContainer;
@@ -21,17 +24,63 @@ public class TutorialZoom extends MFormLayout implements View {
         final FilterableListContainer<Person> container = MyDataSourceGenerator.makeDummyDataset();
 
         final TextZoomField zoomField =
-                new TextZoomField()
+                new TextZoomField("Simple Zoom Field")
                     .withZoomDialog(new TableZoomDialog(TARGET_PROPERTY_ID, container));
+        final Label zoomFieldSelection = new Label();
+        zoomField.addValueChangeListener(new LabelUpdater(zoomFieldSelection));
+
+
+        final TextZoomField zoomFieldNoNull =
+                new TextZoomField("Zoom Field with Null Disabled")
+                        .withNullSelectionDisabled()
+                        .withZoomDialog(new TableZoomDialog(TARGET_PROPERTY_ID,
+                        container));
+        final Label zoomFieldNoNullSelection = new Label();
+
+        zoomFieldNoNull.addValueChangeListener(new LabelUpdater(zoomFieldNoNullSelection));
+
+
+
+        final TextZoomField zoomFieldModeProperty =
+                new TextZoomField("Zoom Field with Property-Only")
+                        .withMode(ZoomField.Mode.PropertyId)
+                        .withZoomDialog(new TableZoomDialog(TARGET_PROPERTY_ID, container));
+
+        final Label zoomFieldModePropertySelection = new Label();
+        zoomFieldModeProperty.addValueChangeListener(new LabelUpdater(zoomFieldModePropertySelection));
+
+
 
         final TextZoomField drillDownField =
-                new TextZoomField()
+                new TextZoomField("DrillDown Only, with Grid")
                     .drillDownOnly()
                     .withZoomDialog(new GridZoomDialog(TARGET_PROPERTY_ID, container));
 
 
-        addComponents(zoomField, drillDownField);
+        addComponents(
+                        zoomField, zoomFieldSelection,
+                        zoomFieldNoNull, zoomFieldNoNullSelection,
+                        zoomFieldModeProperty, zoomFieldModePropertySelection,
+                        drillDownField
+        );
+
+        setMargin(true);
     }
+
+    static class LabelUpdater implements Property.ValueChangeListener {
+        final Label label;
+
+        public LabelUpdater(Label label) {
+            this.label = label;
+        }
+
+        @Override
+        public void valueChange(Property.ValueChangeEvent event) {
+            label.setValue(
+                    "Selection internal value: " + event.getProperty().getValue());
+        }
+
+    };
 
 
     @Override
