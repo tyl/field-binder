5b70439027a818aac359c378c16b89f9fd231361
Edoardo Vacchi <uncommonnonsense@gmail.com>
2015-03-05 15:56:21 +0100

    Migrate bean extractor to more elegant checking
        no more strings comparison in Item class identification

--- field-binder/src/main/java/org/tylproject/vaadin/addon/utils/DefaultBeanExtractor.java
+++ field-binder/src/main/java/org/tylproject/vaadin/addon/utils/DefaultBeanExtractor.java
@@ -3,33 +3,43 @@ package org.tylproject.vaadin.addon.utils;
 import com.vaadin.addon.jpacontainer.EntityItem;
 import com.vaadin.data.Item;
 import com.vaadin.data.util.BeanItem;
+import org.vaadin.viritin.DynaBeanItem;
+import org.vaadin.viritin.ListContainer;
 
 /**
- * Created by evacchi on 10/02/15.
+ * Extracts bean from an Item, depending on the Container implementation
  */
 public class DefaultBeanExtractor implements BeanExtractor {
     @Override
     public Object extract(Object itemId, Item item) throws IllegalArgumentException {
 
         final Class<?> itemClass = item.getClass();
-        final String itemClassName = itemClass.getCanonicalName();
         Object bean;
 
-        // we compare strings so that Java linked does not complain
-        // if any of these classes is missing
-        if (itemClassName.startsWith("org.vaadin.viritin.")) {
+        if (ListContainer.DynaBeanItem.class.isAssignableFrom(itemClass)) {
             bean = itemId;
         } else
-        if (itemClassName.startsWith("com.vaadin.addon.jpacontainer.")) {
+        if (isJPAContainer(itemClass)) {
             bean = ((EntityItem<?>)item).getEntity();
         } else
         if (BeanItem.class.isAssignableFrom(item.getClass())) {
             bean = ((BeanItem<?>) item).getBean();
         } else{
             throw new UnsupportedOperationException(
-                    "Unknown item type: "+itemClassName);
+                    "Unknown item type: "+itemClass.getCanonicalName());
         }
 
         return bean;
     }
+
+
+    protected boolean isJPAContainer(Class<?> itemClass) {
+        try {
+            return EntityItem.class.isAssignableFrom(itemClass);
+        } catch (NoClassDefFoundError ex) { return false; }
+    }
+
+
+
+
 }
